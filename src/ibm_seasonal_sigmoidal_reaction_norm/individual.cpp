66819775a997ac38d3b175390831b6263fce9992
Bram Kuijper <bramkuijper@gmail.com>
2025-09-17 12:33:46 +0100

    adding seasonal sigmoidal

---
+++ src/ibm_seasonal_sigmoidal_reaction_norm/individual.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <random>
+#include <cmath>
+#include "individual.hpp"
+#include "parameters.hpp"
+
+Individual::Individual(Parameters const &params, bool const is_female) :
+    is_female{is_female},
+    a{params.init_a}, // pivotal temp 
+    b{params.init_b}, // temp slope (sigmoidal)
+    t{params.init_t}, // timing intercept
+    tb{params.init_tb}, // timing slope on perceived temp
+    tmax{params.init_tmax} // max to the per-timestep probability
+{}
+
+// copy constructor
+Individual::Individual(Individual const &other) :
+    is_female{other.is_female},
+    attempted_to_mate{other.attempted_to_mate},
+    a{other.a},
+    b{other.b},
+    t{other.t},
+    tb{other.tb},
+    tmax{other.tmax} 
+{
+}
+
+// birth constructor
+Individual::Individual(Individual const &mom,
+        Individual const &dad,
+        Parameters const &par,
+        std::mt19937 &rng_r) :
+    is_female{true} // by default individual is female until sex is determined
+{
+    std::uniform_real_distribution uniform{0.0,1.0};
+    std::normal_distribution normal{0.0, par.sdmu};
+
+    // inherit haploid traits
+    a = uniform(rng_r) < 0.5 ? dad.a : mom.a; // pivotal temperature
+    b = uniform(rng_r) < 0.5 ? dad.b : mom.b; // sex alloc slope on temperature
+    t = uniform(rng_r) < 0.5 ? dad.t : mom.t; // timing of reproduction, intercept
+    tb = uniform(rng_r) < 0.5 ? dad.tb : mom.tb; // timing of reproduction, plasticity
+    tmax = uniform(rng_r) < 0.5 ? dad.tmax : mom.tmax; // timing of reproduction, max prob per timestep
+                                              //
+    // mutate the intercept
+    if (uniform(rng_r) < par.mu_a)
+    {
+        a += normal(rng_r);
+
+        a = std::clamp(a, par.ab_range[0], par.ab_range[1]);
+    }
+
+    // mutate the threshold above which individuals develop as male
+    if (uniform(rng_r) < par.mu_b)
+    {
+        b += normal(rng_r);
+        b = std::clamp(b, par.ab_range[0], par.ab_range[1]);
+    }
+
+    if (uniform(rng_r) < par.mu_t)
+    {
+        t+=normal(rng_r);
+        t = std::clamp(t, par.ab_range[0], par.ab_range[1]);
+    }
+    
+    if (uniform(rng_r) < par.mu_tb)
+    {
+        tb += normal(rng_r);
+        tb = std::clamp(tb, par.ab_range[0], par.ab_range[1]);
+    }
+    
+    if (uniform(rng_r) < par.mu_tmax)
+    {
+        tmax+=normal(rng_r);
+
+        if (tmax < 0)
+        {
+            tmax = 0;
+        }
+    }
+} // end birth constructor
+
+
+// assignment operator
+void Individual::operator=(Individual const &other)
+{
+    is_female = other.is_female;
+    a = other.a;
+    b = other.b;
+    t = other.t;
+    tb = other.tb;
+    tmax = other.tmax;
+    attempted_to_mate = other.attempted_to_mate;
+}
+
+// determine sex of this individual
+double Individual::determine_sex(double const temperature)
+{
+    double p_female = 1.0 / (1.0 + std::exp(-b *(temperature - a)));
+    
+    return(p_female);
+} // end determine_sex
+
---
+++ src/ibm_seasonal_sigmoidal_reaction_norm/vary_parameters.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import socket
+import datetime
+import sys
+import copy
+
+# name of the executable
+exe_name = "./tsd_seasonal.exe"
+
+# get the timestamp to create output file names
+# which will be passed as a command-line argument
+date = datetime.datetime.now()
+base_name = "sim_seasonal_tsd_" +\
+        f"{date:%d}_{date:%m}_{date:%Y}_{date:%H}{date:%M}{date:%S}"
+
+ctr = 0
+max_simulation_time = int(20000000)
+
+sm = [ 0 ]
+sf = [ 0 ]
+
+t_opt_f = [0.5]
+t_opt_m = [0.5,0.8]
+
+# width of the selection function
+omega = 0.01
+
+nrep = 3
+
+temp_change = [0.5]
+mu_t = [0.05]
+
+mu_tb = [0,0.05]
+temp_error = [0,0.01,0.05,0.5,1.0]
+cue_error = [0,0.01,0.05,0.5,1.0]
+
+# aim for roughly 25000 lines
+skip = max([int(round(max_simulation_time / 25000, 0)),1])
+
+for mu_tbi in mu_tb:
+    for change_i in temp_change:
+        for rep_i in range(0,nrep):
+            for sm_i in sm:
+                for sf_i in sf:
+                    if sf_i != sm_i:
+                        continue
+
+                    for t_opt_fi in t_opt_f:
+                        for t_opt_mi in t_opt_m:
+                            for temp_error_i in temp_error:
+                                for cue_error_i in cue_error:
+
+                                    # get the basename
+                                    ctr += 1
+                                    base_name_i = base_name + "_" + str(ctr)
+
+                                    print(f"{exe_name} {base_name_i} " \
+                                            f"{max_simulation_time} " \
+                                            f"{sm_i} " \
+                                            f"{sf_i} " \
+                                            f"{omega} " \
+                                            f"{omega} " \
+                                            f"{t_opt_mi} " \
+                                            f"{t_opt_fi} " \
+                                            f"{skip} " \
+                                            f"{change_i} " \
+                                            f"{mu_t[0]} " \
+                                            f"{mu_tbi} " \
+                                            f"{temp_error_i} " \
+                                            f"{cue_error_i} " \
+                                          )
